50b881f6472c16e4928bac0e67bf0b8d23db6267
Roger Alejandro Gomez Sanabria <rogergomezs2010@outlook.com>
2026-04-24 13:42:54 -0400

    Ignorar archivos temporales y subir proyecto

---
+++ tutorialdotnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tutorialdotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
